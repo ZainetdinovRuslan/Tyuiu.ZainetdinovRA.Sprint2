b4d013008945a3b8c331be62cebe765ec72c082d
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-11-17 04:30:19 +0500

    Task4

--- Tyuiu.ZainetdinovRA.Sprint2.Task4.V10.Lib/DataService.cs
+++ Tyuiu.ZainetdinovRA.Sprint2.Task4.V10.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.ZainetdinovRA.Sprint2.Task4.V10.Lib
     {
         public double Calculate(double x, double y)
         {  
-            return Math.Round(x * 2 < y / 3 ? Math.Pow(1 + 3/Math.Pow(y,2), x) : y + (10*x) - (5/x));
+            return Math.Round(x * 2 < y / 3 ? Math.Pow(1 + 3/Math.Pow(y,2), x) : y + (10*x) - (5/x), 3);
         }
     }
 }
