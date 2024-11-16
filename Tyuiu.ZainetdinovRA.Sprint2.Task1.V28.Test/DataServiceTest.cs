640d6001f7a1dc49892eea89905a778b2bec64cb
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-11-17 02:35:36 +0500

    Task1

---
+++ Tyuiu.ZainetdinovRA.Sprint2.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ZainetdinovRA.Sprint2.Task1.V28.Lib
+{
+    public class DataService : ISprint2Task1V28
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            return new bool[] { c == d | d == c, c != d & d != c, a < b || b < c, b > a && a > c, !(a >= d), d >= a ^ c >= d };
+        }
+    }
+}
---
+++ Tyuiu.ZainetdinovRA.Sprint2.Task1.V28/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Tyuiu.ZainetdinovRA.Sprint2.Task1.V28.Lib;
+namespace Tyuiu.ZainetdinovRA.Sprint2.Task1.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Спринт #2 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !,^, последовательность *");
+            Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив): (True, False,     *");
+            Console.WriteLine("* True, False, True, False), при a = 247, b = 654, c = 671, d = 671       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("* a = 247, b = 654, c = 671, d = 671                                      *");
+
+            int a = 247;
+            int b = 654;
+            int c = 671;
+            int d = 671;
+            bool[] results = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(string.Join(",", results));
+        }
+    }
+}
